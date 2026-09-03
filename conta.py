349403a7c7fe4e9c58fde7ce608209486c58c5d4
gis-koding <giselly.dossantos@dcomp.ufs.br>
2026-09-02 21:14:27 -0300

    fix: CadastroConta, Conta e Main

--- conta.py
+++ conta.py
@@ -1,58 +1,99 @@
 def CadastroConta(nome,cpf):
-    print("Conta Cadastrada com Sucesso!")
-    print("Deseja realizar um depósito de R$50,00 para acessar sua conta?")
-    resposta = input()
-    if resposta == "Sim":
+    
+    print("""Conta Cadastrada com Sucesso!
+Deseja realizar um depósito de R$50,00 para acessar sua conta? 
+s/n""")
+    resposta = input() #confirmação da ação
+
+    #Confirmado
+    if resposta == "s":
         print("Digite o valor a ser depositado:")
         valor = float(input())
-        if valor > 50.00:
+
+        #Para saber se o valor digitado está correto
+        if valor >= 50.00:
             saldo = valor
-            print("Depósito feito com sucesso!")
-            print("Entrando na conta...")
-            print("O Banco Dawn agradece a preferência :)")
-            print("Aguarde...")
+            print("""Depósito feito com sucesso!
+Entrando na conta...""")
+            return saldo
         else:
             saldo = valor
-            print("Depósito feito com sucesso!")
-            print("Não foi possível entrar na conta pois o valor é inferior ao mínimo.")
-            print("Dirija-se a uma agência para desbloquear sua conta.")
-    elif resposta == "Não":
+            print("""Depósito feito com sucesso!
+Não foi possível entrar na conta pois o valor é inferior ao mínimo.""")
+
+    #Cancelado
+    elif resposta == "n":
         print("Conta Bloqueada!")
+        
+    #Qualquer outra resposta 
     else:
         print("Resposta inválida.")
 
-def Conta(saldo):
+
+
+
+def Conta(saldo,nome):
+
+    #Menu de escolha
     print(f"""Conta de {nome}
-    Menu de Funções:
-    Consultar Saldo
-    Realizar Depósito
-    Realizar Saque""")
-    Funcao = input("Digite uma função do menu: ")
-    if funcao == "Consultar Saldo":
-        print("Saldo = {saldo}")
-    elif funcao == "Realizar Depósito":
+Menu de Serviços:
+1- Saldo
+2- Depósito
+3- Saque
+4- Sair""")
+    funcao = int(input("Digite o número do serviço: "))
+
+    #Saldo
+    if funcao == 1:
+        print(f"Saldo = R${saldo}")
+
+    #Depósito
+    elif funcao == 2:
+    
         print("Digite o valor para depositar: ")
         valor = float(input())
-        print(f"Deseja depositar o valor de {valor} na sua conta?")
+        print(f"""Deseja depositar o valor de R${valor} na sua conta?
+s/n""") #sim ou não, confirmar ou cancelar
+
         confirmacao = input()
-        if confirmacao == "Sim":
-            saldo = saldo +  valor
+        #Confirmado
+        if confirmacao == "s":
+            saldo += valor
             print("Seu depósito foi concluído com sucesso!")
-            print(f"Saldo Atual: R${saldo}")
-        elif confirmacao == "Não":
+            print(f"Saldo atual: R${saldo}")
+        #Cancelado
+        elif confirmacao == "n":
             print("Depósito Cancelado")
+        #QR
         else: 
             print("Resposta inválida.")
-    elif funcao == "Realizar Saque":
+
+    #Saque
+    elif funcao == 3:
+    
         print("Qual valor deseja retirar?")
         valor = float(input())
-        print(f"Deseja retirar o valor de {valor}?")
+        print(f"""Deseja retirar o valor de R${valor}?
+s/n""")
+
         confirmacao = input()
-        if confirmacao == "Sim":
-            saldo = saldo - valor
-            print("Saque realizado com sucesso!")
-        elif confirmacao == "Não":
+        if confirmacao == "s":
+            if valor < saldo:
+                saldo = saldo - valor
+                print("Saque realizado com sucesso!")
+                print(f"Saldo atual: R${saldo}")
+            else:
+                print("Saldo insuficiente ou valor não existe.")
+        elif confirmacao == "n":
             print("Saque não realizado.")
         else:
             print("Resposta inválida.")
+            
+    #Sair
+    elif funcao == 4:
+        print("Saindo da conta...")
+        
+    #QR
+    else:
+        print("Resposta inválida.")
             
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from conta import *
+from cliente import *
 
+nome, cpf = CadastroCliente()
+validacao = ValidarCPF(cpf)
+
+if validacao:
+    saldo = CadastroConta(cliente,cpf)
+    
+    if saldo >= 50:
+        Conta(saldo,cliente)
