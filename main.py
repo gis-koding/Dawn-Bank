6de2a891262a61a8e286275c9892b03d6c7b868b
Julyana Lima <julyanalima.ai@gmail.com>
2026-09-15 23:32:50 -0300

    feat: main recebe nova função - AdicionarCliente

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from conta import *
 from cliente import *
 
 #cliente 374.443.990-93
+#listas com os dados dos clientes
+lista_nomes = []
+lista_cpfs = [] 
 
 nome, cpf = CadastroCliente()
 validacao = ValidarCPF(cpf)
+AdicionaCliente(lista_nomes, lista_cpfs, nome, cpf) #cliente validado e adicionados nas listas
 
 if validacao:
     saldo = CadastroConta(nome,cpf)
     
     if saldo >= 50:
         Conta(saldo,nome,cpf)
+        
+
