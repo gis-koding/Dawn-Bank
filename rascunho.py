107188724f0025769128da4b2a04bebfb38f57c2
Giselly Marianny <giselly.dossantos@dcomp.ufs.br>
2026-09-24 23:05:27 -0300

    fix: rascunho

--- rascunho.py
+++ rascunho.py
@@ -1,23 +1,7 @@
-
-import json 
-from biblioteca import *
-
-clientes = acessar_lista("dados json/clientes.json")
-for i in clientes:
-    print(i)
-
-contas = acessar_lista("dados json/contas.json")
-for j in contas:
-    print(j)
-
-saldos = acessar_lista("dados json/saldos.json")
-for k in saldos:
-    print(k)
-
 '''Lógica Inicial:
 agencia 001 = 3 primeiros
 agencia 002 = restante
 
-Em Breve:
+ou:
 agencia 001 = tipo(conta) corrente
 agencia 002 = tipo(conta) poupança'''
